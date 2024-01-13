5e7dd840b80fd8eece35048cfc1874185e12eaa3
Sam Payne <10paynes@gmail.com>
2024-01-13 19:20:23 +0200

    130124 Updates 1.4

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ dailyElement.addEventListener("change", function () {
   hourElement.classList.remove("bg-indigo-600");
   dailyElement.classList.add("bg-indigo-600");
 });
+
+document.addEventListener('scroll', function () {
+  var header = document.querySelector('header');
+  var scrollPosition = window.scrollY;
+
+  if (scrollPosition > 50) { // Adjust the scroll position threshold as needed
+    header.classList.remove('nav-bg-initial');
+    header.classList.add('nav-bg-scrolled');
+  } else {
+    header.classList.remove('nav-bg-scrolled');
+    header.classList.add('nav-bg-initial');
+  }
+});
