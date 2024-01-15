3f04d212effdbc5443e2be0666422f3fc81e710a
Sam Payne <10paynes@gmail.com>
2024-01-15 17:31:44 +0200

    130124 Updates 2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,24 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    screens: {
+      xs: "475px",
+      sm: '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
     extend: {
-        animation: {
-          'loop-scroll': 'loop-scroll 80s linear infinite',
+      animation: {
+        "loop-scroll": "loop-scroll 80s linear infinite",
+      },
+      keyframes: {
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
         },
-        keyframes: {
-          'loop-scroll': {
-            from: { transform: 'translateX(0)' },
-            to: { transform: 'translateX(-100%)' },
-          }
-        }
+      },
     },
   },
-  plugins: [
-    require("tailwindcss-animation-delay"),
-  ],
+  plugins: [require("tailwindcss-animation-delay")],
 };
