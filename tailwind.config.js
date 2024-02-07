11a4d5b22ead4c5abd523d256f8eb5f881f2f323
Sam Payne <10paynes@gmail.com>
2024-02-08 01:56:28 +0200

    Updates 8th Feb

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,26 @@ module.exports = {
     extend: {
       animation: {
         "loop-scroll": "loop-scroll 80s linear infinite",
+        "blob": "blob 7s infinite", // Adding the blob animation
       },
       keyframes: {
         "loop-scroll": {
           from: { transform: "translateX(0)" },
           to: { transform: "translateX(-100%)" },
+        },
+        "blob": {  // Defining the blob keyframes
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
         },
       },
     },
